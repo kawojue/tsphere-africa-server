f46c5eac92cd858efa90492e720cc6e93c61a064
Raheem Kawojue <kawojue08@gmail.com>
2024-03-22 10:45:04 +0100

    migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
-import { FileModule } from './file/file.module'
+// import { FileModule } from './file/file.module'
 import { AppController } from './app.controller'
 import { CreativeModule } from './creative/creative.module'
 
 @Module({
   imports: [
     AuthModule,
-    FileModule,
+    // FileModule,
     CreativeModule
   ],
   controllers: [AppController],
