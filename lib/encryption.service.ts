5492ac9fe68ea16f602c1470ada65223b8a20660
Raheem Kawojue <kawojue08@gmail.com>
2024-04-11 17:07:51 +0100

    del: pkg

--- lib/encryption.service.ts
+++ lib/encryption.service.ts
@@ -1,11 +1,8 @@
 import * as bcrypt from 'bcrypt'
-import * as core from 'decrypt-core'
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class EncryptionService {
-    private readonly ENCRYPTION_KEY = process.env.HANDLE_ENCRYPTION_KEY
-
     async hashAsync(password: string, saltRounds: number = 10): Promise<string> {
         const salt = await bcrypt.genSalt(saltRounds)
         return await bcrypt.hash(password, salt)
@@ -13,13 +10,5 @@ export class EncryptionService {
 
     async compareAsync(plain: string | Buffer, hashed: string): Promise<boolean> {
         return await bcrypt.compare(plain, hashed)
-    }
-
-    cipherSync(plain: string) {
-        return core.encrypt(plain, this.ENCRYPTION_KEY)
-    }
-
-    decipherSync(encryted: string) {
-        return core.decrypt(encryted, this.ENCRYPTION_KEY)
     }
 }
