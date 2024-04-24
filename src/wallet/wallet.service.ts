86530a2b34ece5b9bc5279a266fab782e5b3668f
Raheem Kawojue <kawojue08@gmail.com>
2024-04-24 16:07:06 +0100

    chore: wallet & tx history model

--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -4,6 +4,7 @@ import { MiscService } from 'lib/misc.service'
 import { BrevoService } from 'lib/brevo.service'
 import { PrismaService } from 'lib/prisma.service'
 import { PaystackService } from 'lib/Paystack/paystack.service'
+import { TxHistory, TxStatus, TxType } from '@prisma/client'
 
 @Injectable()
 export class WalletService {
@@ -15,5 +16,55 @@ export class WalletService {
         private readonly paystack: PaystackService,
     ) { }
 
+    async manageTransferEvents(body: TransferEvent) {
+        const data = body.data
+        try {
+            const transaction = await this.getTransaction(data.reference)
 
+            if (transaction) {
+                await this.updateTransactionStatus(transaction, data.status as TxStatus)
+
+                const amount = this.calculateTotalAmount(data.amount, transaction.totalFee)
+
+                if (body.event === 'transfer.reversed' || body.event === 'transfer.failed') {
+                    await this.updateUserBalance(transaction.userId, amount)
+                    await this.updateTransactionType(transaction, 'REVERSED')
+                }
+            }
+        } catch (err) {
+            throw err
+        }
+    }
+
+    private async getTransaction(reference: string) {
+        return await this.prisma.txHistory.findUnique({
+            where: { reference }
+        })
+    }
+
+    private async updateTransactionStatus(tx: TxHistory, status: TxStatus) {
+        await this.prisma.txHistory.update({
+            where: { id: tx.id },
+            data: { status }
+        })
+    }
+
+    private async updateTransactionType(tx: TxHistory, type: TxType) {
+        await this.prisma.txHistory.update({
+            where: { id: tx.id },
+            data: { type }
+        })
+    }
+
+    private calculateTotalAmount(amount: number, totalFee: number) {
+        const KOBO = 100 as const
+        return (amount / KOBO) + totalFee
+    }
+
+    private async updateUserBalance(userId: string, amount: number) {
+        await this.prisma.wallet.update({
+            where: { userId },
+            data: { balance: { increment: amount } }
+        })
+    }
 }
