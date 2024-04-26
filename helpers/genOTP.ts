7157fc78ba44b13fa6ae503f296277d4feadb09c
Raheem Kawojue <kawojue08@gmail.com>
2024-04-27 00:58:26 +0100

    chore: totp

--- helpers/genOTP.ts
+++ helpers/genOTP.ts
@@ -1,19 +1,17 @@
 const generateOTP = (length: number = 6) => {
-    let totp: string = ''
+    let otp: string = ''
     const digits: string = '0123456789'
+
     for (let i = 0; i < length; i++) {
-        totp += digits[Math.floor(Math.random() * length)]
+        otp += digits[Math.floor(Math.random() * length)]
     }
 
     const currentDate: Date = new Date()
-    const expiryDate: Date = new Date(
-        currentDate.setMinutes(currentDate.getMinutes() + 5)
+    const otp_expiry: Date = new Date(
+        currentDate.setMinutes(currentDate.getMinutes() + 10)
     )
 
-    return {
-        totp,
-        totp_expiry: expiryDate.toISOString()
-    }
+    return { otp, otp_expiry }
 }
 
 export default generateOTP
