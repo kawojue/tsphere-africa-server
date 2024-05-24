6980d24453341207831d490b262654b6d4921ae1
Raheem Kawojue <kawojue08@gmail.com>
2024-05-24 12:11:07 +0100

    ..

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
+      'http://localhost:3001',
       `http://localhost:${PORT}`,
       'https://talentsphereafrica.co',
       'https://talentsphereafrica.com',
